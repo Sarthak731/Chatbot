b4ff16c2181d4eecc2df4366f5483e00e2e74f32
Sarthak Chaurasia <121442487+Sarthak731@users.noreply.github.com>
2024-10-23 11:10:30 +0530

    script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let chatContainer = document.querySelector(".chat-container")
 let btn = document.querySelector(".btn")
 let userMessage = null
 
-const Api_url='https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyBV0lb3uaqbtCpnPxXzDIewpN1wCxBOK-0'
+const Api_url='https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=YOUR API KEY'
 function createChatBox(html, className) {
   const div = document.createElement("div")
   div.classList.add(className)
